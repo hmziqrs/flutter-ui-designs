3b9f3d445bb4a1a17574ab9c4dbde33f63561dbb
hackerhgl <hackerhgl@gmail.com>
2020-07-05 15:22:00 +0500

    Updated linux binaries & Built vslaunch script in dart

--- scripts/utils.dart
+++ scripts/utils.dart
@@ -6,3 +6,20 @@ String normalize(String path) {
   }
   return path;
 }
+
+dynamic mkDir(String path, {bool logs = false}) {
+  try {
+    final file = Directory(path);
+    final check = file.existsSync();
+    if (logs) {
+      print('path: $path');
+      print('check: $check');
+    }
+    if (!check) {
+      return file.createSync();
+    }
+    return check;
+  } catch (e) {
+    throw e;
+  }
+}
---
+++ scripts/vslaunch.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'utils.dart' as utils;
+
+const List<String> desktopFilters = ['mac', 'linux', 'windows'];
+
+void main(List<String> args) async {
+  final result = await Process.run("flutter", ["devices"]);
+  final List<String> raw = result.stdout.split('\n');
+
+  print(raw.runtimeType);
+
+  final devices = raw.sublist(2, raw.length - 1).map((result) {
+    final List<String> arr =
+        result.split('•').map((str) => str.trim()).toList();
+    final String name = arr[0];
+    final String deviceId = arr[1];
+
+    final Map<String, dynamic> obj = {
+      "name": name,
+      "deviceId": deviceId,
+      "type": 'dart',
+      "request": 'launch',
+    };
+
+    final check = desktopFilters.indexWhere(
+      (str) => str.toLowerCase().contains(
+            deviceId.toLowerCase(),
+          ),
+    );
+
+    if (check == -1) {
+      obj["args"] = ['-t', 'lib/main.mobile.dart'];
+    }
+    return obj;
+  });
+
+  final Map<String, dynamic> newConfig = {
+    'version': '1.0.0',
+    'configurations': ([
+      {
+        'name': 'Flutter',
+        'request': 'launch',
+        'type': 'dart',
+      },
+      ...devices,
+    ]),
+    'compounds': [
+      {
+        'name': 'current',
+        'configurations': devices.map((obj) => obj["name"]).toList(),
+      },
+    ],
+  };
+
+  utils.mkDir('.vscode');
+  final vsConfig = new File('.vscode/launch.json');
+  final encoded = json.encode(newConfig);
+  vsConfig.writeAsStringSync(encoded);
+
+  print(devices);
+}
