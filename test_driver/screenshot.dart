e1e545294415033bca074da16119424fbf16fd4d
hackerhgl <hackerhgl@gmail.com>
2020-07-30 08:32:36 +0500

    Stable Windows screenshot integration

--- test_driver/screenshot.dart
+++ test_driver/screenshot.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 
 abstract class Screenshot {
@@ -9,15 +11,33 @@ abstract class Screenshot {
       case "linux":
         await screenshotLinux(label);
         break;
+      case "windows":
+        screenshotWindows(label);
+        break;
       default:
     }
   }
 
-  static Future<void> screenshotLinux(
-    String label,
-  ) async {
+  static Future<void> screenshotLinux(String label) async {
     try {
       print("screenName $label");
     } catch (e) {}
+  }
+
+  static void screenshotWindows(String label) {
+    try {
+      final libPath = "..\\..\\test_driver\\libs\\screenshot-windows.exe";
+      final directory = "screenshots\\windows";
+
+      Process.runSync(
+        libPath,
+        [label],
+        runInShell: true,
+        workingDirectory: directory,
+      );
+      // await Future.delayed(Duration(seconds: 1));
+    } catch (e) {
+      print("ERROR CAN'T TAKE $label screenshot");
+    }
   }
 }
