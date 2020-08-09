0188bc09fe8ffe751874e98942ae2ef4487f4b35
hackerhgl <hackerhgl@gmail.com>
2020-08-10 00:40:55 +0500

    Cleaned up testing files

--- test_driver_dummy/app.dart
+++ test_driver_dummy/app.dart
@@ -1,13 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_uis/io/io.dart';
+
 import 'package:flutter_uis/main.dart' as app;
 
-import 'utils.dart';
-
-void main(List<String> args) async {
+main(List<String> args) {
   final navigationObserver = NavigatorObserver();
 
   enableFlutterDriverExtension(
@@ -40,20 +37,4 @@ void main(List<String> args) async {
   );
 
   app.mainTest(navigationObserver);
-
-  // When I maximize windows after test driver connects with the app.
-  // It wasn't working my actions were failing.
-  // Maybe driver save dimensions & offets of window frame and widgets at the initial connection.
-  // And maybe changing windows frame size were messing up with the initial data.
-  // That's why I had to maximize windows before driver connects with the app instance.
-  // So driver saves the proper data from start.
-  const platform = String.fromEnvironment("PLATFORM");
-  int delay = 0;
-
-  if (platform == "windows") {
-    Utils.initMaxWindowsCmdow();
-    delay = 8;
-  }
-  // This is for safety so app is rendered properly before test driver connects with the app instance
-  await Future.delayed(Duration(seconds: delay));
 }
