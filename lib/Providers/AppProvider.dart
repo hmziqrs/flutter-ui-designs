de1120dbe1294733c5d03708d63df9bca1d0c8d9
hackerhgl <hackerhgl@gmail.com>
2020-11-08 06:12:18 +0500

    Theme Mode & Locale caching integration

--- lib/Providers/AppProvider.dart
+++ lib/Providers/AppProvider.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:hive/hive.dart';
+
+final themeMap = {
+  "system": ThemeMode.system,
+  "dark": ThemeMode.dark,
+  "light": ThemeMode.light,
+};
+
+enum Cache {
+  theme,
+  locale,
+}
 
 class AppProvider extends ChangeNotifier {
   static AppProvider state(BuildContext context, [bool listen = false]) =>
       Provider.of<AppProvider>(context, listen: listen);
-
-  Locale _activeLocale;
 
   static List<Locale> locales = [
     Locale('en', 'US'),
@@ -13,10 +23,44 @@ class AppProvider extends ChangeNotifier {
     Locale('ar', 'SA'),
   ];
 
+  static final appBox = Hive.box('app');
+
+  AppProvider() {
+    final cachedLocale = appBox.get(Cache.locale.toString());
+    if (cachedLocale != null) {
+      final split = cachedLocale.split("_");
+      this._activeLocale = Locale(split.first, split.last);
+    }
+
+    final cachedTheme = appBox.get(Cache.theme.toString());
+    this._themeMode =
+        cachedTheme == null ? themeMap["system"] : themeMap[cachedTheme];
+  }
+
+  Locale _activeLocale;
   Locale get activeLocale => this._activeLocale;
 
   setActiveLocale(Locale locale) {
+    if (locale == this._activeLocale) {
+      return;
+    }
     this._activeLocale = locale;
+    appBox.put(Cache.locale.toString(), locale.toString());
     notifyListeners();
+  }
+
+  ThemeMode _themeMode = ThemeMode.system;
+  ThemeMode get themeMode => this._themeMode;
+
+  void setTheme(ThemeMode newTheme) {
+    if (this._themeMode == newTheme) {
+      return;
+    }
+    this._themeMode = newTheme;
+    notifyListeners();
+    appBox.put(
+      Cache.theme.toString(),
+      newTheme.toString().split(".").last,
+    );
   }
 }
--- lib/main.mobile.dart
+++ lib/main.mobile.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive/hive.dart';
 
 import 'package:flutter_uis/Navigator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('app');
+
   final analyticsObeserver = FirebaseAnalyticsObserver(
     analytics: FirebaseAnalytics(),
   );
