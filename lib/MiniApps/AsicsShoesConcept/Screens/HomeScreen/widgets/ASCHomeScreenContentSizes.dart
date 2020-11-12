9b46e52a19fc647d799a64413f76d24a0128af82
hackerhgl <hackerhgl@gmail.com>
2020-11-12 18:09:02 +0500

    MiniApp ASC shoe widiget optimization

--- lib/MiniApps/AsicsShoesConcept/Screens/HomeScreen/widgets/ASCHomeScreenContentSizes.dart
+++ lib/MiniApps/AsicsShoesConcept/Screens/HomeScreen/widgets/ASCHomeScreenContentSizes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_uis/MiniApps/AsicsShoesConcept/Screens/HomeScreen/Provider.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'package:supercharged/supercharged.dart';
 
@@ -25,8 +26,6 @@ class ASCHomeScreenContentSizes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final state = ASCShoeProvider.state(context, true);
-
     final tween = MultiTween<AniColors>()
       ..add(
         AniColors.bg,
@@ -39,53 +38,59 @@ class ASCHomeScreenContentSizes extends StatelessWidget {
         250.milliseconds,
       );
 
-    return Row(
-      children: ASCShoeProvider.shoeSizes
-          .asMap()
-          .entries
-          .map(
-            (entry) => Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: AppDimensions.padding * 2,
-              ),
-              child: GestureDetector(
-                onTap: () => state.setShoeSize(entry.value),
-                key: Key(
-                  ASCHomeScreenTestKeys.getSize(
-                    activePage,
-                    entry.key + 1,
+    return Selector<ASCShoeProvider, int>(
+      selector: (_, s) => s.activeShoeSize,
+      builder: (context, activeShoeSize, child) {
+        return Row(
+          children: ASCShoeProvider.shoeSizes.asMap().entries.map(
+            (entry) {
+              final isActive = activeShoeSize == entry.value;
+              return Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppDimensions.padding * 2,
+                ),
+                child: GestureDetector(
+                  onTap: () => ASCShoeProvider.state(
+                    context,
+                  ).setShoeSize(entry.value),
+                  key: Key(
+                    ASCHomeScreenTestKeys.getSize(
+                      activePage,
+                      entry.key + 1,
+                    ),
+                  ),
+                  child: CustomAnimation<MultiTweenValues<AniColors>>(
+                    tween: tween,
+                    duration: tween.duration,
+                    key: Key(App.isDark().toString()),
+                    control: isActive
+                        ? CustomAnimationControl.PLAY
+                        : CustomAnimationControl.PLAY_REVERSE,
+                    builder: (context, child, animation) {
+                      return Container(
+                        width: Dimensions.sizeRadius,
+                        height: Dimensions.sizeRadius,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: animation.get(AniColors.bg),
+                        ),
+                        child: Text(
+                          entry.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: animation.get(AniColors.text),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                 ),
-                child: CustomAnimation<MultiTweenValues<AniColors>>(
-                  tween: tween,
-                  duration: tween.duration,
-                  key: Key(App.isDark().toString()),
-                  control: state.activeShoeSize == entry.value
-                      ? CustomAnimationControl.PLAY
-                      : CustomAnimationControl.PLAY_REVERSE,
-                  builder: (context, child, animation) {
-                    return Container(
-                      width: Dimensions.sizeRadius,
-                      height: Dimensions.sizeRadius,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: animation.get(AniColors.bg),
-                      ),
-                      child: Text(
-                        entry.value.toString(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: animation.get(AniColors.text),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          )
-          .toList(),
+              );
+            },
+          ).toList(),
+        );
+      },
     );
   }
 }
